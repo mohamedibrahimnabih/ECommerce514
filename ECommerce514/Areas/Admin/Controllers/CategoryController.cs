2fafd77cb34070ab55fe4ece89deb935c36f2bff
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-13 12:21:01 +0300

    CRUD operation on category Model

---
+++ ECommerce514/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using ECommerce514.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce514.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var result = _context.Categories;
+
+            return View(result.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if(category is not null)
+            {
+                return View(category);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category is not null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+        }
+    }
+}
--- ECommerce514/Areas/Customer/Controllers/HomeController.cs
+++ ECommerce514/Areas/Customer/Controllers/HomeController.cs
@@ -1,17 +1,16 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using ECommerce514.Models;
 using ECommerce514.ViewModels;
 using ECommerce514.Data;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 
-namespace ECommerce514.Controllers;
+namespace ECommerce514.Areas.Customer.Controllers;
 
+[Area("Customer")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly ApplicationDbContext _context = new();
+    private ApplicationDbContext _context = new();
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -35,14 +34,14 @@ public class HomeController : Controller
 
         if (productsWithFilterVM.MinPrice > 0)
         {
-            product = product.Where(e => e.Price - (e.Price * (decimal)(e.Discount / 100.0)) >= (decimal)productsWithFilterVM.MinPrice);
+            product = product.Where(e => e.Price - e.Price * (decimal)(e.Discount / 100.0) >= productsWithFilterVM.MinPrice);
             //ViewData["minPrice"] = minPrice;
             ViewBag.minPrice = productsWithFilterVM.MinPrice;
         }
 
         if (productsWithFilterVM.MaxPrice > 0)
         {
-            product = product.Where(e => e.Price - (e.Price * (decimal)(e.Discount / 100.0)) <= (decimal)productsWithFilterVM.MaxPrice);
+            product = product.Where(e => e.Price - e.Price * (decimal)(e.Discount / 100.0) <= productsWithFilterVM.MaxPrice);
             //ViewData["maxPrice"] = maxPrice;
             ViewBag.maxPrice = productsWithFilterVM.MaxPrice;
         }
@@ -86,11 +85,11 @@ public class HomeController : Controller
 
         if(product is not null)
         {
-            var relatedProduct = _context.Products.Where(e => e.CategoryId == product.CategoryId && id != e.ProductId).Skip(0).Take(4);
+            var relatedProduct = _context.Products.Include(e => e.Category).Where(e => e.CategoryId == product.CategoryId && id != e.ProductId).Skip(0).Take(4);
 
-            var topProduct = _context.Products.Where(e => e.ProductId != id).OrderByDescending(e => e.Traffic).Skip(0).Take(4);
+            var topProduct = _context.Products.Include(e=>e.Category).Where(e => e.ProductId != id).OrderByDescending(e => e.Traffic).Skip(0).Take(4);
 
-            var similarProductsName = _context.Products.Where(e=>e.Name.Contains(product.Name) && id != e.ProductId).Skip(0).Take(4);
+            var similarProductsName = _context.Products.Include(e => e.Category).Where(e=>e.Name.Contains(product.Name) && id != e.ProductId).Skip(0).Take(4);
 
             product.Traffic++;
             _context.SaveChanges();
@@ -104,7 +103,12 @@ public class HomeController : Controller
             });
         }
 
-        return NotFound();
+        return RedirectToAction(nameof(NotFoundPage));
+    }
+
+    public IActionResult NotFoundPage()
+    {
+        return View();
     }
 
     public IActionResult Privacy()
