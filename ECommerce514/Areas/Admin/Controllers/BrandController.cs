67db01e7816f7a3576120cb1fe9ae8ff4134915d
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-13 14:23:15 +0300

    CRUD operation on brand Model

---
+++ ECommerce514/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,68 @@
+﻿using ECommerce514.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce514.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var result = _context.Brands;
+
+            return View(result.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            if(brand is not null)
+            {
+                return View(brand);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Brand brand)
+        {
+            _context.Brands.Update(brand);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            if (brand is not null)
+            {
+                _context.Brands.Remove(brand);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+        }
+    }
+}
